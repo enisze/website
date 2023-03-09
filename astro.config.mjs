36f948613bc8139869fe4ad5ee4be54f017cecbc
Enis <eniszej@gmail.com>
2023-03-10 00:27:24 +0100

    ⚗️ Add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx(), tailwind()],
-})
+  integrations: [mdx(), tailwind(), react()]
+});
---
+++ src/components/Hero.tsx
@@ -0,0 +1,44 @@
+import { motion } from 'framer-motion'
+
+export function Hero(props: any) {
+    return (
+        <section className={`relative w-full h-screen mx-auto bg-slate-800`}>
+            <div
+                className={`absolute inset-0 top-[120px] max-w-7xl mx-auto  flex flex-row items-start gap-5`}
+            >
+                <div className="flex flex-col justify-center items-center mt-5">
+                    <div className="w-5 h-5 rounded-full bg-[#915EFF]"></div>
+                    <div className="w-1 h-80 violet-gradient"></div>
+                </div>
+
+                <div>
+                    <h1 className={`text-white`}>
+                        Hi, I'm <span className="text-[#915EFF]">Adrian</span>
+                    </h1>
+                    <p className="mt-2 text-white-100">
+                        I develop 3D visuals, user <br className="block" />
+                        interfaces and web applications
+                    </p>
+                </div>
+            </div>
+
+            <div className="absolute bottom-32 w-full flex justify-center items-center">
+                <a href="#about">
+                    <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+                        <motion.div
+                            animate={{
+                                y: [0, 24, 0],
+                            }}
+                            transition={{
+                                duration: 1.5,
+                                repeat: Infinity,
+                                repeatType: 'loop',
+                            }}
+                            className="w-3 h-3 rounded-full bg-secondary mb-1"
+                        />
+                    </div>
+                </a>
+            </div>
+        </section>
+    )
+}
