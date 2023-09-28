7b2149ad39c53436dfdf58ff6517083dd91273df
Enis <eniszej@gmail.com>
2023-09-29 01:43:44 +0200

    done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
     integrations: [
         mdx(),
         tailwind(),
@@ -12,4 +14,5 @@ export default defineConfig({
             experimentalReactChildren: true,
         }),
     ],
+    adapter: vercel(),
 })
