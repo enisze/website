1bdc3c0048f31eea48ed23cd4a1ab3206db857eb
Enis <eniszej@gmail.com>
2023-10-04 01:17:46 +0200

    🔍

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,24 @@
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
 import { defineConfig } from 'astro/config'
 
+import robotsTxt from 'astro-robots-txt'
+
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
+    site: 'https://website-enisze.vercel.app/',
     integrations: [
         mdx(),
         tailwind(),
         react({
             experimentalReactChildren: true,
         }),
+        sitemap(),
+        robotsTxt(),
     ],
     adapter: vercel(),
 })
