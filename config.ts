32bab91598da814e5259b3bbe1f734c8dcb11fb1
Enis <eniszej@gmail.com>
2023-10-15 22:47:59 +0200

    🔍

--- config.ts
+++ config.ts
@@ -1,6 +1,9 @@
 // @ts-ignore
 const VERCEL_URL = process.env.VERCEL_URL
 
-export const BASE_URL = VERCEL_URL
-    ? `https://${VERCEL_URL}`
-    : `http://localhost:3000`
+const VERCEL_ENV = process.env.VERCEL_ENV
+
+export const BASE_URL =
+    VERCEL_ENV === 'production'
+        ? `https://zejnilovic.de`
+        : `http://localhost:3000`
