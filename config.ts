0db5e426e7ffdd2c9d9555134a6021036795981a
Enis <eniszej@gmail.com>
2023-10-15 22:46:34 +0200

    ⚗️

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-const VERCEL_URL = import.meta.env.NEXT_PUBLIC_VERCEL_URL
+const VERCEL_URL = process.env.VERCEL_URL
 
 export const BASE_URL = VERCEL_URL
     ? `https://${VERCEL_URL}`
