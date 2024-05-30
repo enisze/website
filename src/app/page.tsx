03a9b6febb67e4bc9c920b4fe187a674c1bd0ed4
Enis <eniszej@gmail.com>
2024-05-30 22:35:13 +0200

    comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { Skills } from '../pageParts/skills'
 
 //TODO: Sitemap, robots, themeprovider for dark & lightmode
 // some padding between the page parts add twitter handle
+//Fix mailing
 export default function Home() {
   return (
     <div className='flex flex-col gap-y-3 scroll-mt-32'>
