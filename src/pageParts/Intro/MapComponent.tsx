cebad76f048c86cdbb31e607cc1057423858011b
Enis <eniszej@gmail.com>
2025-04-07 01:18:10 +0200

    💄

--- src/pageParts/Intro/MapComponent.tsx
+++ src/pageParts/Intro/MapComponent.tsx
@@ -16,6 +16,26 @@ L.Icon.Default.mergeOptions({
 		'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png'
 })
 
+// Add this custom icon creation before the AnimatedZoom component
+const createHouseIcon = () => {
+	return L.divIcon({
+		className: 'custom-house-icon',
+		html: `
+			<div class="relative">
+				<div class="absolute -top-4 -left-4 w-8 h-8 bg-blue-500 rounded-full opacity-25 marker-pulse"></div>
+				<div class="absolute -top-3 -left-3 w-6 h-6 bg-blue-500 rounded-full flex items-center justify-center text-white">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+						<path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+						<path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+					</svg>
+				</div>
+			</div>
+		`,
+		iconSize: [30, 30],
+		iconAnchor: [15, 15]
+	})
+}
+
 const AnimatedZoom: React.FC = () => {
 	const map = useMap()
 
@@ -46,7 +66,7 @@ const MapComponent: React.FC = () => {
 						attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>'
 						noWrap
 					/>
-					<Marker position={[50.9375, 6.9603]} />
+					<Marker position={[50.9375, 6.9603]} icon={createHouseIcon()} />
 					<AnimatedZoom />
 				</MapContainer>
 			</div>
